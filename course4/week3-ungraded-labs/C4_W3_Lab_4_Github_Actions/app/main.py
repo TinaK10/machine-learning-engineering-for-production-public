0a73321298236ba92b99657561d20391f7c45694
TinaK10 <117384089+TinaK10@users.noreply.github.com>
2023-01-06 15:58:31 +0100

    Update main.py

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+#Comment to trigger GitHub Action C4_W3_Lab_4_Github_Actions
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
